bb44926f4392526ef750862eedd76a4ab817df24
ajraduenz <aldoraduenzjunior@gmail.com>
2020-07-12 14:09:55 -0300

    Implementado hora final

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var horaAnterior = ""
+var novaHora = ""
 function Calcular() {
     var hora = document.getElementById("hora")
     if (!hora.value) {
@@ -40,12 +42,40 @@ function Calcular() {
             }
             if (Number(minutofinal2) < 10) {
                 minutofinal2 = "0" + String(minutofinal2)
+
+            }
+
+            novaHora = horafinal2 + ":" + minutofinal2
+            somarHora = somaHora(String(horaAnterior), String(novaHora));
+            function somaHora(hr1, hr2) {
+                temp = 0;
+                novaH = 0;
+                novoM = 0;
+                hora1 = Number(hr1.substr(0, 2))
+                hora2 = Number(hr2.substr(0, 2))
+                minu1 = Number(hr1.substr(3, 2))
+                minu2 = Number(hr2.substr(3, 2))
+
+                temp = minu1 + minu2;
+                while (temp > 59) {
+                    novaH++;
+                    temp = temp - 60;
+                }
+                novoM = temp.toString().length == 2 ? temp : ("0" + temp);
+
+                temp = hora1 + hora2 + novaH;
+
+                novaH = temp.toString().length == 2 ? temp : ("0" + temp);
+                horaAnterior = novaH + ":" + novoM;
+                return novaH + ":" + novoM;
             }
 
             res1.innerHTML = (`<p><input type="button" value="Zerar Hora" id="Zerar" onclick="Zerar()"></p>`)
             res1.innerHTML += (`<p><input type="button" value="Zerar Tudo" id="ZerarTudo" onclick="ZerarTudo()"></p>`)
-            res2.innerHTML += (`${porcentagem}% de ${String(f0[0])}:${String(f0[1])} equivale a ${(horadaporcentagem2)}:${(mindaporcentagem2)}. <p><b>TOTAL ${(horafinal2)}:${(minutofinal2)}</b></p>`)
+            res2.innerHTML += (`${porcentagem}% de ${String(f0[0])}:${String(f0[1])} equivale a ${(horadaporcentagem2)}:${(mindaporcentagem2)}. <p>Total: ${(horafinal2)}:${(minutofinal2)}</p>`)
             //res1.innerHTML += (`<p><input type="button" value="Adicionar ${(horafinal2)}:${(minutofinal2)} ao meu banco de horas" id="Banco" onclick="Banco()"></p>`)
+            res3.innerHTML = (`<b>Total Final ${somarHora}</b>`)
+
 
         }
     }
@@ -60,14 +90,18 @@ function Zerar() {
     hora.focus()
 
 }
-function ZerarTudo(){
+function ZerarTudo() {
     var hora = document.getElementById("hora")
     var res1 = document.getElementById("res1")
     var res2 = document.getElementById("res2")
+    var res3 = document.getElementById("res3")
     var porcentagem = Number(document.getElementById("porcentagem").value)
     hora.value = ""
     res1.innerHTML = ""
     res2.innerHTML = ""
+    res3.innerHTML = ""
+    horaAnterior = ""
+    novaHora = ""
     hora.focus()
 
 }
